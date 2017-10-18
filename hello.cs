1122990c5672d2ad52befe186ff799215627befb
lukon10 <32896086+lukon10@users.noreply.github.com>
2017-10-18 11:38:42 +0200

    Update hello.cs

--- hello.cs
+++ hello.cs
@@ -1,4 +1,7 @@
 static void Main()
 }
     Console.WriteLine("Hello world!");
+    Console.WriteLine("\nWhat's your name?");
+    string s = Console.ReadLine();
+    Console.WriteLine("Hello {0}!",s);
 }
